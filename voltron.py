a809689a266c0b867c852f8f90adbe566aa65aa2
justin <justin@voltcreative.com>
2021-07-01 12:55:37 -0400

    Initial push

---
+++ voltron.py
@@ -0,0 +1,39 @@
+import slack_sdk
+import os
+import json
+from pathlib import Path
+from flask import Flask, request, Response
+from dotenv import dotenv_values
+from slackeventsapi import SlackEventAdapter
+
+config = dotenv_values(".env")
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(config["SLACK_SECRET"],'/slack/events',app)
+
+client = slack_sdk.WebClient(token=config["SLACK_TOKEN"])
+BOT_ID = client.api_call('auth.test')['user_id']
+
+print(config)
+
+@app.route("/send-message", methods=['POST'])
+def send_message():
+    json_data = json.loads(str(request.data, encoding='utf-8'))
+    channel = json_data["channel"]
+    text = json_data["text"]
+    client.chat_postMessage(channel=channel, text=text)
+    return Response(), 200
+
+@app.route("/yell-at-me", methods=['POST'])
+def yell_at_me():
+    data = request.form
+    print(data)
+    user = data["user_name"]
+    userId = data["user_id"]
+    channel = data["channel_name"]
+    text = "HEY, " + user.upper() + ", GET BENT"
+    client.chat_postMessage(channel=userId, text=text)
+    return Response(), 200
+
+if (__name__ == "__main__"):
+    app.run(debug=True)
