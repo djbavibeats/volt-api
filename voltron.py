393736fdd9baafd1b6e90c75af65cc6c3e6dfeee
justin <justin@voltcreative.com>
2021-07-01 13:16:40 -0400

    Initial commit

--- voltron.py
+++ voltron.py
@@ -43,4 +43,5 @@ def yell_at_me():
     return Response(), 200
 
 if (__name__ == "__main__"):
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
