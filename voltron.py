46ef952d39802af3a23f21113bece2f18690f0a2
justin <justin@voltcreative.com>
2021-07-01 13:42:34 -0400

    Initial commit

--- voltron.py
+++ voltron.py
@@ -3,17 +3,22 @@ import os
 import json
 from pathlib import Path
 from flask import Flask, request, Response
-from dotenv import dotenv_values
+from dotenv import dotenv_values, load_dotenv
 from slackeventsapi import SlackEventAdapter
 
+load_dotenv()
 config = dotenv_values(".env")
 
 app = Flask(__name__)
-# slack_event_adapter = SlackEventAdapter(config["SLACK_SECRET"],'/slack/events',app)
-slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
 
-# client = slack_sdk.WebClient(token=config["SLACK_TOKEN"])
-client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
+if os.environ["PRODUCTION"] == True:
+    slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
+    client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
+    host = '0.0.0.0'
+else:
+    slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
+    client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
+    host = '127.0.0.1'
 
 BOT_ID = client.api_call('auth.test')['user_id']
 
@@ -21,7 +26,7 @@ print(config)
 
 @app.route('/')
 def warning():
-    return "<div style='text-align: center; margin: 0 auto;'><img src='https://www.dropbox.com/s/ogtx35aiia317ye/steve%21.jpg?dl=0' /><h3>you shouldn't be here...</h3></div>"
+    return "<div style='height: 100vh; display: flex; flex-direction: column; justify-content: center; text-align: center; margin: 0 auto;'><img style='width: 65%; margin: 0 auto;' src='https://previews.dropbox.com/p/thumb/ABNkfTnO0XCrAYYpqQ4dVxhaqordD1In-fbI7tx_MNOgeB-ILyt9r-hcuMgKX4hR9KLsMG9XA870e2zqJxZmUOAa9ecWd_ZqsePJCsf5FRspiU-iHDQQph3XEF2R9lrK4VpWEBSAq7eAtdYPyIeJovFBtKlBiqhddBUpsyPLjcBYNS8tDHNj-k-YX9BXePgG4O433cMeoUDMhG3kihcc1IvdiXZjI7qTJ5XNgdz-VChCzCVY8OxZlK0-Qdu9v7uTUfJA4RvhGB2dKMuvphYF8qMlnCDD3DIiZviojCatW-CKlc5zTw3aE5mAX3J2uwXbjCobNK3tX8L5Jqp23EJiEL4gRTmrZi0cEs2svHIJ7MW6Aw/p.jpeg?fv_content=true&size_mode=5' /><h1>you shouldn't be here...</h1></div>"
 
 @app.route("/send-message", methods=['POST'])
 def send_message():
@@ -44,4 +49,4 @@ def yell_at_me():
 
 if (__name__ == "__main__"):
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host=host, port=port)
