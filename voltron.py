18cced994cdff2fe30cdcc40a3f1593a53fdcf06
djbavibeats <justin@voltcreative.com>
2021-09-22 17:05:31 -0400

    Digital Ocean

--- voltron.py
+++ voltron.py
@@ -1,4 +1,4 @@
-import slack_sdk
+# import slack_sdk
 import os
 import json
 from pathlib import Path
@@ -6,7 +6,7 @@ from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 from flask_login import login_user, logout_user, current_user
 from dotenv import load_dotenv
-from slackeventsapi import SlackEventAdapter
+# from slackeventsapi import SlackEventAdapter
 from db.models import User, Project
 from db import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -21,21 +21,21 @@ app = create_app()
 CORS(app)
 
 if os.environ["PRODUCTION"] == "True":
-    slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
-    client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
+    # slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
+    # client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
     host = '0.0.0.0'
 else:
-    slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
-    client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
+    # slack_event_adapter = SlackEventAdapter(os.environ["SLACK_SECRET"],'/slack/events',app)
+    # client = slack_sdk.WebClient(token=os.environ["SLACK_TOKEN"])
     host = '127.0.0.1'
 
-BOT_ID = client.api_call('auth.test')['user_id']
+# BOT_ID = client.api_call('auth.test')['user_id']
 
 print(host)
 
 @app.route('/')
 def warning():
-    return "<div style='overflow: hidden; height: 100vh; display: flex; flex-direction: column; justify-content: center; text-align: center; margin: 0 auto;'><img style='width: 65%; margin: 0 auto;' src='https://previews.dropbox.com/p/thumb/ABNkfTnO0XCrAYYpqQ4dVxhaqordD1In-fbI7tx_MNOgeB-ILyt9r-hcuMgKX4hR9KLsMG9XA870e2zqJxZmUOAa9ecWd_ZqsePJCsf5FRspiU-iHDQQph3XEF2R9lrK4VpWEBSAq7eAtdYPyIeJovFBtKlBiqhddBUpsyPLjcBYNS8tDHNj-k-YX9BXePgG4O433cMeoUDMhG3kihcc1IvdiXZjI7qTJ5XNgdz-VChCzCVY8OxZlK0-Qdu9v7uTUfJA4RvhGB2dKMuvphYF8qMlnCDD3DIiZviojCatW-CKlc5zTw3aE5mAX3J2uwXbjCobNK3tX8L5Jqp23EJiEL4gRTmrZi0cEs2svHIJ7MW6Aw/p.jpeg?fv_content=true&size_mode=5' /><h1>you shouldn't be here...</h1></div>"
+    return "<div style='overflow: hidden; height: 100vh; display: flex; flex-direction: column; justify-content: center; text-align: center; margin: 0 auto;'><img style='width: 65%; margin: 0 auto;' src='https://www.dropbox.com/s/ogtx35aiia317ye/steve%21.jpg?raw=0' /><h1>you shouldn't be here...</h1></div>"
 
 # AUTH ROUTES
 @app.route("/sign-up", methods=['POST'])
@@ -163,24 +163,24 @@ def update_project():
 # END PROJECT ROUTES
 
 # SLACK ROUTES
-@app.route("/send-message", methods=['POST'])
-def send_message():
-    json_data = json.loads(str(request.data, encoding='utf-8'))
-    channel = json_data["channel"]
-    text = json_data["text"]
-    client.chat_postMessage(channel=channel, text=text)
-    return Response({ "nice" }), 200
+# @app.route("/send-message", methods=['POST'])
+# def send_message():
+#     json_data = json.loads(str(request.data, encoding='utf-8'))
+#     channel = json_data["channel"]
+#     text = json_data["text"]
+#     client.chat_postMessage(channel=channel, text=text)
+#     return Response({ "nice" }), 200
 
-@app.route("/yell-at-me", methods=['POST'])
-def yell_at_me():
-    data = request.form
+# @app.route("/yell-at-me", methods=['POST'])
+# def yell_at_me():
+#     data = request.form
 
-    user = data["user_name"]
-    userId = data["user_id"]
-    channel = data["channel_name"]
-    text = "HEY, " + user.upper() + ", GET BENT"
-    client.chat_postMessage(channel=userId, text=text)
-    return Response(), 200
+#     user = data["user_name"]
+#     userId = data["user_id"]
+#     channel = data["channel_name"]
+#     text = "HEY, " + user.upper() + ", GET BENT"
+#     client.chat_postMessage(channel=userId, text=text)
+#     return Response(), 200
 # END SLACK ROUTES
 
 if (__name__ == "__main__"):
